f244615d23b051c71c1c57f67fd6be5b697a11c8
Alexsys <43126944+alexandephilia@users.noreply.github.com>
2026-03-07 17:41:11 +0000

    clov 0.29.0

--- Formula/clov.rb
+++ Formula/clov.rb
@@ -1,21 +1,21 @@
 class Clov < Formula
   desc "Clov Token Omitter - High-performance CLI proxy to minimize LLM token consumption"
   homepage "https://github.com/alexandephilia/clov-ai"
-  version "0.28.0"
+  version "0.29.0"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/alexandephilia/clov-ai/releases/download/v0.28.0/clov-aarch64-apple-darwin.tar.gz"
-    sha256 "564ba0c702acf62c825c62c0ee040eadbec7858f73b8e023d803104476feb1a9"
+    url "https://github.com/alexandephilia/clov-ai/releases/download/v0.29.0/clov-aarch64-apple-darwin.tar.gz"
+    sha256 "5c635693a6682acedcc774ba391963105b8d9a214d2a1bf7c7f674ba49e540cd"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/alexandephilia/clov-ai/releases/download/v0.28.0/clov-x86_64-apple-darwin.tar.gz"
-    sha256 "3270f35ff202c3fca3060f723ca376433229651cacf6ed7b7c4a2e04b93e5f98"
+    url "https://github.com/alexandephilia/clov-ai/releases/download/v0.29.0/clov-x86_64-apple-darwin.tar.gz"
+    sha256 "5bd06331cfc83e89f3be1681e6d76b3f53ea80a82b41f0311db72fbc19f11bb8"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/alexandephilia/clov-ai/releases/download/v0.28.0/clov-aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "4a28679464f77896c484183ad884122a1007467eaef03e0830bf0f42f34737b8"
+    url "https://github.com/alexandephilia/clov-ai/releases/download/v0.29.0/clov-aarch64-unknown-linux-gnu.tar.gz"
+    sha256 "d5ef62846dab91ebaa7dc844fed588596c1c60ffdb7c7ef54eabfcb25bda6db6"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/alexandephilia/clov-ai/releases/download/v0.28.0/clov-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "1cbcbd6c6633dffecbd57ea76fd72a858c79ccf8ada46515146eacb589947425"
+    url "https://github.com/alexandephilia/clov-ai/releases/download/v0.29.0/clov-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "5dc8f30d0bf86a0b40b21c338189f237135717060a6977d51ff654a8efd89b43"
   end
 
   def install
