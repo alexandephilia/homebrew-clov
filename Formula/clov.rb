0a2405d1f70f7df8208e6047beeeac245dc545cd
Alexsys <43126944+alexandephilia@users.noreply.github.com>
2026-03-08 12:19:00 +0000

    clov 0.35.0

--- Formula/clov.rb
+++ Formula/clov.rb
@@ -6,16 +6,16 @@ class Clov < Formula
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/alexandephilia/clov-ai/releases/download/v0.35.0/clov-aarch64-apple-darwin.tar.gz"
-    sha256 "0c96d5854426b54cb304fc68891c628e5d8405434086d550e59798f0e498d1c0"
+    sha256 "45e120644be7ced4b4f938085139dc1f444b61a37beae1510b1f59f6eaefeb83"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/alexandephilia/clov-ai/releases/download/v0.35.0/clov-x86_64-apple-darwin.tar.gz"
-    sha256 "60751ef395fce2c0fb6cf0b2bebad0cb946b133f15bf292bd9dd4db171cbdd27"
+    sha256 "dc69084707820c131f086a0be572b6f4200bd31d71967a8c1aba66e759ad43b1"
   elsif OS.linux? && Hardware::CPU.arm?
     url "https://github.com/alexandephilia/clov-ai/releases/download/v0.35.0/clov-aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "48bcb8dfbfbbb5adaf3499c1bb40d1387a12291b2d9efef0dd7db93656f33d07"
+    sha256 "582e0b9cde4bbd58d30cf2025a1e579ca427c55c179d16d357a5e60b59963f9d"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/alexandephilia/clov-ai/releases/download/v0.35.0/clov-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "4a0f80082aaa3f76b728827db9535700d89c5fcae81dc55b5b763d34016b0b25"
+    sha256 "73c84172d87785000fbb1ce1859ae7b392df1211ab7f13e983ab43143fd2880b"
   end
 
   def install
